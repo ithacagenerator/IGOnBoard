307fec304f061d713140fa03ae2f7d4c12544c89
Victor Aprea <victor.aprea@wickeddevice.com>
2020-01-25 12:31:52 -0500

    progress check

--- ExpressApp/assign-coupon-codes.js
+++ ExpressApp/assign-coupon-codes.js
@@ -1,6 +1,8 @@
 // jshint esversion: 8
 const argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
+const { generateCouponCode } = require('./routes/couponCodes');
+const { findDocuments, updateDocument } = require('./util/db');
 if (!argv.members) {
   console.log('must provide members file path with --members');
   process.exit(-1);
@@ -9,4 +11,27 @@ if (!argv.members) {
 const membersdata = fs.readFileSync(argv.members);
 const members = JSON.parse(membersdata);
 
-console.log(members);
+if (!Array.isArray(members)) {
+  console.log('members file must, but does not, contain an array');
+  process.exit(-2);
+}
+
+if (members.length <= 0) {
+  console.log('members file contains an empty array');
+  process.exit(-3);
+}
+
+members.forEach(member => {
+  if (!member.email) {
+    console.log('members file records should contain an email', member);
+    process.exit(-4);
+  }
+
+  if (!members[0].name) {
+    console.log('members file records should contain a name', member);
+    process.exit(-5);
+  }
+});
+
+// ok now we're all good
+console.log(generateCouponCode, updateDocument, findDocuments);
